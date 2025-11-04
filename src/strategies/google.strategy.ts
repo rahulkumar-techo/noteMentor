eebef9b7fe4b2938e85d04fed3f5910c50f280dc
Rahul Kumar <rahulkumar7250883289@gmail.com>
2025-11-04 11:32:54 +0530

    google auth problem

--- src/strategies/google.strategy.ts
+++ src/strategies/google.strategy.ts
@@ -13,7 +13,7 @@ passport.use(
     {
       clientID: config.google.google_clientId,
       clientSecret: config.google.google_secret,
-      callbackURL: `${process.env.CALLBACK_URL}/auth/google/callback`,
+      callbackURL: `${process.env.SERVER_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
