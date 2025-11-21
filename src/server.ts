e09984684cbbf721c0e23b1bfb1f4b6cea567421
Rahul Kumar <rahulkumar7250883289@gmail.com>
2025-11-21 08:25:06 +0530

    fix:g-callback5

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
-import cluster from "cluster";
-import os from "os";
-import process from "process";
 import app from "./app";
 import { db_connection } from "./config/db.config";
 import { config } from "./config/env.config";
 import { log } from "./shared/logs/logger";
-import { Server } from "socket.io";
 import { createServer } from "node:http";
 import { initSocket } from "./socket";
 
 const PORT = config.port || 8000;
-const isPrimary = cluster.isPrimary;
-const totalCPUs = Math.max(os.cpus().length - 1, 1);
 
 process.on("uncaughtException", (err) => {
   log.error(`Uncaught Exception: ${err.message}`);
@@ -23,50 +17,33 @@ process.on("unhandledRejection", (reason: any) => {
   log.error(`Unhandled Rejection: ${reason}`);
 });
 
-if (isPrimary) {
-  log.info(`Master PID: ${process.pid}`);
-  log.info(`Spawning ${totalCPUs} workers...`);
+(async () => {
+  try {
+    await db_connection();
+    log.info(`💽 Connected to MongoDB successfully`);
 
-  for (let i = 0; i < totalCPUs; i++) cluster.fork();
+    const httpServer = createServer(app);
 
-  cluster.on("exit", (worker, code, signal) => {
-    log.error(
-      `Worker ${worker.process.pid} exited (code: ${code}, signal: ${signal}). Restarting...`
-    );
-    cluster.fork();
-  });
+    // Initialize socket.io
+    const io = initSocket(httpServer);
 
-} else {
-  (async () => {
-    try {
-      await db_connection();
-      log.info(`🔗Worker ${process.pid}: Database connected.`);
+    httpServer.listen(PORT, () => {
+      log.info(`🚀 Server running on port ${PORT} (${config.nodeEnv})`);
+    });
 
-      const httpServer = createServer(app);
-
-      // initialize socket.io
-      const io = initSocket(httpServer);
-
-      httpServer.listen(PORT, () => {
-        log.info(
-          `🖥️ Worker ${process.pid} running on port ${PORT} (${config.nodeEnv})`
-        );
+    const gracefulShutdown = () => {
+      log.warn(`Server shutting down...`);
+      httpServer.close(() => {
+        log.info(`Server stopped.`);
+        process.exit(0);
       });
+    };
 
-      const gracefulShutdown = () => {
-        log.warn(`Worker ${process.pid} shutting down...`);
-        httpServer.close(() => {
-          log.info(`Worker ${process.pid} stopped.`);
-          process.exit(0);
-        });
-      };
+    process.on("SIGTERM", gracefulShutdown);
+    process.on("SIGINT", gracefulShutdown);
 
-      process.on("SIGTERM", gracefulShutdown);
-      process.on("SIGINT", gracefulShutdown);
-
-    } catch (error: any) {
-      log.error(`Worker ${process.pid} failed: ${error.message}`);
-      process.exit(1);
-    }
-  })();
-}
+  } catch (error: any) {
+    log.error(`Server failed: ${error.message}`);
+    process.exit(1);
+  }
+})();
